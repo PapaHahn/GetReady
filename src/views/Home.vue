0b2747db1cc553c9afd75d9fddb701ca2d57d01e
PapaHahn <chahn.business@gmail.com>
2025-01-03 02:45:54 +0100

    edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,12 @@ const navigateToCounter = () => {
 
 
 <template>
-    <div class=" w-screen h-screen text-center justify-center flex flex-col items-center bg-cover grayscale bg-center" :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <div class=" w-screen h-screen text-center justify-center flex flex-col items-center bg-cover grayscale bg-center sm:bg-contain" :style="{ backgroundImage: `url(${backgroundImage})` }">
             
         <div class="absolute inset-0 bg-black opacity-50 ">
         </div>
         <div class="z-10">
-            <h1 class="text-9xl text-white font-extrabold">ARE YOU READY?</h1>
+            <h1 class="text-9xl sm:text-4xl text-white font-extrabold">ARE YOU READY?</h1>
             <button 
             class="bg-white text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded-full mt-4 sm:mt-6 md:mt-10" 
             @click="navigateToCounter">
